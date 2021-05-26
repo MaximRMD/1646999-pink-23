cb42af43e8d9c7c1291a6b74f9dd5d97bb30d08f
Maksim <maxrmd@yandex.ru>
2021-05-26 23:55:05 +0300

    finish#2

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,13 +1,19 @@
-var navMain = document.querySelector(".main-nav");
-var navButtonOpen = document.querySelector(".nav-toggle_open");
-var navButtonClose = document.querySelector(".nav-toggle_closed");
+const navMain = document.querySelector(".main-nav");
+const navButtonOpen = document.querySelector(".nav-toggle_open");
+const navButtonClose = document.querySelector(".nav-toggle_closed");
+const headerTop = document.querySelector(".header__top");
 
-navMain.classList.remove("main-nav_nojs");
+headerTop.classList.remove("header__top_nojs");
+
 
 navButtonOpen.addEventListener("click", function() {
   navMain.classList.remove("main-nav_closed");
+  headerTop.classList.remove("header__top_close");
+  headerTop.classList.add("header__top_open");
 });
 
 navButtonClose.addEventListener("click", function() {
   navMain.classList.add("main-nav_closed");
+  headerTop.classList.add("header__top_close");
+  headerTop.classList.remove("header__top_open");
 });
